d0f6bf033f0973594c9a730247bfd3d85882cb90
Konstantine <konstantinev@protonmail.com>
2019-09-17 22:45:28 -0500

    Added a fourth Fizzbuzz

---
+++ src/testing/fizzbuzz_4.cr
@@ -0,0 +1,16 @@
+class Testing::Fizzbuzz_3
+
+	# Another version I've seen for crystal
+	1.upto(100) do |v|
+		p fizz_buzz(v)
+	end
+	
+	def fizz_buzz(value)
+		word = ""
+		word += "fizz" if value % 3 == 0
+		word += "buzz" if value % 5 == 0
+		word += value.to_s if word.empty?
+		word
+	end
+
+end
