d3f6ab34e5035824c5b4506565f4509d545fbec4
Konstantine <konstantinev@protonmail.com>
2019-09-25 23:14:11 -0500

    Init for Fractal code

---
+++ src/testing/fractal.cr
@@ -0,0 +1,12 @@
+require 'stumpy_png'
+
+module Fractals
+  # Code Here
+
+  # Render test code in stumpy_png
+
+  # Define positions, min max, etc
+  # Create classes for Mandelbrot and Julia
+  # Render code in stumpy_png
+
+end
