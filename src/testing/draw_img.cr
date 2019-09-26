eb0aa681b12026d3607ef7915d7b7e582a90f29b
Konstantine <konstantinev@protonmail.com>
2019-09-25 23:38:23 -0500

    Testing out stumpy_png

--- src/testing/draw_img.cr
+++ src/testing/draw_img.cr
@@ -1,8 +1,17 @@
 require "stumpy_png"
 
-module Fractals
+class CheckImageFoo
+  
+  canvas = StumpyPNG.read("img/foo.png")
+  # Set to relative path since I run from the parent directory
+  r, g, b = canvas[0, 0].to_rgb8
+  puts "red=#{r}, green=#{g}, blue=#{b}"
+  # This should output the RGB values from 0,0 in the image.
 
-  # Render test code in stumpy_png
+end
+
+
+class DrawImgRainbow
   include StumpyPNG
   canvas = Canvas.new(256, 256)
   (0..255).each do |x|
@@ -14,9 +23,4 @@ module Fractals
     end
   end
   StumpyPNG.write(canvas, "rainbow.png")
-
-  # Define positions, min max, etc
-  # Create classes for Mandelbrot and Julia
-  # Render code in stumpy_png
-
 end
