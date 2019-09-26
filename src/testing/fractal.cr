d41c3c6d4d864e1b4b2ff6e244752833900530e9
Konstantine <konstantinev@protonmail.com>
2019-09-25 23:33:15 -0500

    Added test image and test code

--- src/testing/fractal.cr
+++ src/testing/fractal.cr
@@ -1,9 +1,15 @@
-require 'stumpy_png'
+require "stumpy_png"
 
 module Fractals
   # Code Here
 
   # Render test code in stumpy_png
+
+  canvas = StumpyPNG.read("img/foo.png")
+  # Set to relative path since I run from the parent directory
+  r, g, b = canvas[0, 0].to_rgb8
+  puts "red=#{r}, green=#{g}, blue=#{b}"
+  # This should output the RGB values from 0,0 in the image.
 
   # Define positions, min max, etc
   # Create classes for Mandelbrot and Julia
