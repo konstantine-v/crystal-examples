c7c9b347e925511cbbf7062974b7642817090c5a
Konstantine <konstantinev@protonmail.com>
2019-10-01 21:35:30 -0500

    Added metric meaurement and if statement for that

--- src/testing/bmi_calculate.cr
+++ src/testing/bmi_calculate.cr
@@ -1,29 +1,48 @@
 
 class BmiCalulate
-
-  if (set_weight === "Male") {
-    total_BMI = (set_weight * 703) / (set_height * set_height)
-  } elsif (set_weight === "Male") {
-    total_BMI = (set_weight * 703) / (set_height * set_height)
-  } else {
-    total_BMI = "Error, something went wrong..."
-  }
   
 end
 
 class BmiValues
 
-  possible_genders = ["Male", "Female"] #For BMI, don't get mad
-  set_gender = "Male"
-  set_age    = "23"
-  set_height = "5.11"
-  set_weight = "180"
+  # Allow for user input
+  # Change set to get once inputs are created
 
+  possible_genders = ["Male", "Female"] # For BMI, don't get mad
+  set_gender = possible_genders.first()
+  set_age = 23
+  possible_units = ["Imperial", "Metric"] # Prompt user for type of units
+  set_unit_of_measure = possible_units.first()
+
+  # Hardcode values in for testing
+  if (set_unit_of_measure == "Imperial") {
+    set_height = 5.11       # Feet
+    set_weight = 180        # Pounds
+    set_height_in = set_height * 12
+    total_BMI = (set_weight / (set_height_in * set_height_in)) * 703
+  } elsif (set_unit_of_measure == "Metric") {
+    set_height = 155.7528   # Centimeters
+    set_weight = 81.648     # Kilos
+    total_BMI = set_weight / (set_height_in * set_height_in)
+  } else {
+    puts "error"
+    break
+  }
+
+  # Convert imp to metric for whatever reason
+  # set_height_metric = set_height * 2.54
+  # set_weight_metric = set_weight * 0.4536
+
+  # Print Outputs - Include Chart with output
   puts "Below 18.5 =	Underweight"
   puts "18.5 - 24.9 =	Normal"
   puts "25 - 29.9 =	Overweight"
   puts "30.0 + =	Obese"
   puts "======================"
   puts " Your BMI is: #{total_BMI}"
+
+  # Advanced:
+    # Allow for outputting all data input into JSON format
+    # Add Calculations for Male and Female added
 
 end
