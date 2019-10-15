63504a2a1109ab42aa879fdcb7fa678d2ee13f0c
Konstantine <konstantinev@protonmail.com>
2019-10-14 22:52:50 -0500

    Finished quote generator and added db

--- src/alan_watts_quotes.cr
+++ src/alan_watts_quotes.cr
@@ -1,18 +1,15 @@
 require "sqlite3"
 
-# Put instructions to get the db with wget or something
 class Quotes
-  DB.open "sqlite3://./lib/quotes.db" do |db|
+  DB.open "sqlite3://./data/quotes.db" do |db|
+    # db in data directory
     puts "Quotes:"
-    db.query "select AuthorName, QuoteText from Authors as a inner join Quotes as b on a.AuthorId = b.AuthorId order by AuthorName" do |rs|
-      puts "#{rs.column_name(0)} - #{rs.column_name(1)}"
-      # => AuthorName - QuoteText
-      rs.each do
-        puts "#{rs.read(String)} - '#{rs.read(String)}'"
+    db.query "select AuthorName, QuoteText from Authors as a inner join Quotes as b on a.AuthorId = b.AuthorId order by random() limit 1" do |data|
+      data.each do
+        puts "#{data.read(String)} - '#{data.read(String)}'"
       end
     end
+    
+
   end
-
 end
-
-# Add Admiral shard to make this so much better. Maybe do that in a seperate project and include db.
