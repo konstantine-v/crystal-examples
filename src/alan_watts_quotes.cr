35d5bf230c93bbc81936b8a36d6f14e6935c42b5
Konstantine <konstantinev@protonmail.com>
2019-10-16 13:17:11 -0500

    Cleanup of quotes script

--- src/alan_watts_quotes.cr
+++ src/alan_watts_quotes.cr
@@ -2,14 +2,10 @@ require "sqlite3"
 
 class Quotes
   DB.open "sqlite3://./data/quotes.db" do |db|
-    # db in data directory
-    puts "Quotes:"
     db.query "select AuthorName, QuoteText from Authors as a inner join Quotes as b on a.AuthorId = b.AuthorId order by random() limit 1" do |data|
       data.each do
-        puts "#{data.read(String)} - '#{data.read(String)}'"
+        pp "#{data.read(String)} - '#{data.read(String)}'"
       end
     end
-    
-
   end
 end
